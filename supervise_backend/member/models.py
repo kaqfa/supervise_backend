f28a5274ce81fefcf9a7a51eaabdbdb293ffa293
fahri firdausillah <elfaatta@gmail.com>
2016-11-28 07:10:41 +0700

    model sampe template

--- supervise_backend/member/models.py
+++ supervise_backend/member/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Expertise(models.Model):
+    """Bidang keahlian supervisor"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+
+
+class Member(models.Model):
+    """Pengganti User, include student dan supervisor"""
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    nim = models.CharField(max_length=20, null=True)
+    npp = models.CharField(max_length=20, null=True)
+    name = models.CharField(max_length=10, null=True)
+    address = models.TextField(null=True)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.CharField(max_length=200)
+    expertise = models.ManyToManyField(Expertise, null=True)
+    level = models.CharField(max_length=2, default='st')
+    status = models.CharField(max_length=1, default='a')
+
+
+class MemberToken(models.Model):
+    """Token untuk login"""
+    member = models.ForeignKey(Member)
+    token = models.CharField(max_length=20)
+    status = models.CharField(max_length=1)
+    
--- supervise_backend/progress/models.py
+++ supervise_backend/progress/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from member.models import Member, Expertise 
 
-# Create your models here.
+
+class MediaFile(models.Model):
+    filename = models.FileField()
+    by = models.ForeignKey(Member)
+    upload_date = models.DateField()
+
+
+class Thesis(models.Model):
+    topic = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    abstract = models.TextField(null=True)
+    student = models.ForeignKey(Member)
+    field = models.ManyToManyField(Expertise)
+    files = models.ManyToManyField(MediaFile)
+    save_date = models.DateField()
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    duration = models.SmallIntegerField()
+    files = models.ManyToManyField(MediaFile)
+
+
+class Template(models.Model):
+    code = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    task = models.ManyToManyField(Task)
